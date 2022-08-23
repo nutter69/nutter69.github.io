de8bcf3a28d837a8a7c7a9ff96cad2a96abf8b44
nutter69 <89165518+nutter69@users.noreply.github.com>
2022-08-22 21:00:41 -0400

    Update nut.js

--- nut.js
+++ nut.js
@@ -13,43 +13,49 @@ let g = [
         cost: 5000,
         amt: 0,
         cm: 3,
-        }
+        },
+    g3 = {
+        cost: 6.9e6,
+        amt:0,
+        cm:4,
+    }
 ];
 setInterval(() => {
     nut += g[0].amt;
     ew.textContent = Math.round(nut).toString();
     g.forEach((gen , i) => {
-        switch (i) {
-            case 0:
-                nuts.textContent = `Cost: ${Math.round(g[i].cost)}`;
-                break;
-                case 1:
-                    g[i-1].amt += g[i].amt / 10;
-                    if (g[i-1].amt >= 10) {
-                        nuts.textContent = `Cost: ${Math.round(g[i].cost)}`;
-                    }
-                    break;
-        }
+    if (i > 0)
+    {
+        g[i-1].amt += g[i].amt / 10;
+        if (g[i-1].amt >= 10) {
+             nuts.textContent = `Cost: ${Math.round(g[i].cost)}`;
+         }
+    }
+    else
+    {
+        nuts.textContent = `Cost: ${Math.round(g[i].cost)}`;
+    }
         gens[i].textContent = Math.round(gen.amt).toString();
     });
 } , 1);
 nuts.addEventListener("click" , () => {
     g.forEach((gen , i) => {
-        switch (i) {
-            case 0:
-                if (nut >= g[i].cost && g[i].amt < 10) {
-                    nut -= g[i].cost;
-                    g[i].cost *= g[i].cm;
-                    g[i].amt++;
-                }
-                break;
-                case 1:
-                    if (g[i-1].amt >= 10 && nut >= g[i].cost) {
-                        nut -= g[i].cost;
-                        g[i].cost *= g[i].cm;
-                        g[i].amt++;
-                    }
-                    break;
+        if (i>0)
+        {
+            if (g[i-1].amt >= 10 && nut >= g[i].cost) {
+                nut -= g[i].cost;
+                g[i].cost *= g[i].cm;
+                g[i].amt++;
+            }
+        }
+        else
+        {
+            if (nut >= g[i].cost && g[i].amt < 10)
+            {
+                nut -= g[i].cost;
+                g[i].cost *= g[i].cm;
+                g[i].amt++;
+            }
         }
     });
 });
