a9253568e5749653edb544ec4c0df34ac62b4dc4
nutter69 <89165518+nutter69@users.noreply.github.com>
2022-08-28 19:38:41 -0400

    Update nut.js

--- nut.js
+++ nut.js
@@ -1,6 +1,8 @@
-const nuts = document.querySelector("#b");
-const ew = document.querySelector("#h");
+const nuts = document.querySelector("#buttons");
+const ew = nuts.querySelector("#h");
 const h = document.getElementById("d");
+let st = document.getElementById("stats").querySelectorAll("h1");
+let bn = nuts.querySelectorAll("button");
 let gens = h.querySelectorAll("h1");
 let nut = new ExpantaNum("2");
 let g = [
@@ -10,127 +12,130 @@ let g = [
     cm: ExpantaNum(1.5),
     },
 ];
-function short(x) {
-    let a = ["M","B","T","Qa","Qi","Sx","Sp","Oc","No"];
-    let b = ["","U","D","T","Qu","Qi","Sx","Sp","Oc","No"];
-    let c = ["","De","Vg","Tg","Qug","Qig","Sxg","Spg","Ocg","Nog","Ce"]
-    let p = "";
-    let px = "";
-    let dec = x;
-    if (Math.floor(Math.log10(x)) < 6) {
-        switch (Math.floor(Math.log10(x))) {
-            case 0:
-            px = dec.substring(0,1);
-            return `${px}`;
-            break;
-            case 1:
-                px = dec.substring(0,2);
-                return `${px}`;
-                break;
-                case 2:
-                    px = dec.substring(0,3);
-                    return `${px}`;
-                    break;
-                    case 3:
-                        p = dec.substring(1,4);
-                        px = dec.substring(0,1);
-                        return `${px},${p}`;
-                        break;
-                        case 4:
-                            p = dec.substring(2,5);
-                            px = dec.substring(0,2);
-                            return `${px},${p}`;
-                            break;
-                            case 5:
-                                p = dec.substring(3,6);
-                                px = dec.substring(0,3);
-                                return `${px},${p}`;
-                                break;
-        }
-    }
-    else if (Math.floor(Math.log10(x)) < 18)
-    {
-        switch (Math.floor(Math.log10(x)) % 3) {
-            case 0:
-            p = dec.substring(1,4);
-            px = dec.substring(0,1);
-            break;
-            case 1:
-                p = dec.substring(2,4);
-                px = dec.substring(0,2);
-                break;
-                case 2:
-                    p = dec.substring(3,4);
-                    px = dec.substring(0,3);
-                    break;
-        }
-        return `${px}.${p} ${a[Math.floor(Math.log10(x)/3)-2]}`;
-    }
-    else
-    {
-        switch (Math.floor(Math.log10(x)) % 3) {
-            case 0:
-            p = dec.substring(2,5);
-            px = dec.substring(0,1);
-            break;
-            case 1:
-                p = dec.substring(2,3);
-                px = dec.substring(0,1);
-                px = px.concat(p);
-                p = dec.substring(3,5);
-                break;
-                case 2:
-                    p = dec.substring(2,4);
-                    px = dec.substring(0,1);
-                    px = px.concat(p);
-                    p = dec.substring(4,5);
-                    break;
-        }
-        return `${px}.${p} ${b[Math.floor(Math.log10(x)/3)%10]}${c[Math.floor(Math.log10(x)/30)]}`;
-    }
-}
+let gr = [
+    g1 = {
+    cost: ExpantaNum(100),
+    amt: ExpantaNum(1),
+    cm: ExpantaNum(1.5),
+    },
+];
+let s = [
+    r = new ExpantaNum(1),
+    p = new ExpantaNum(1)
+];
+let sn = ["Rebirth","Prestige"];
 setInterval(() => {
-    nut = ExpantaNum.add(nut,g[0].amt);
+    nut = ExpantaNum.add(nut,ExpantaNum.pow(g[0].amt,s[0]));
     ew.textContent = nut.toPrecision(3);
     g.forEach((gen , i) => {
     if (i > 0)
     {
         g[i-1].amt = ExpantaNum.add(g[i-1].amt,g[i].amt);
         if (ExpantaNum.gte(g[i-1].amt,10)) {
-             nuts.textContent = `Cost: ${g[i].cost.toPrecision(3)}`;
+             bn[0].textContent = `Cost: ${g[i].cost.toPrecision(3)}`;
          }
     }
     else
     {
-        nuts.textContent = `Cost: ${g[i].cost.toPrecision(3)}`;
+        bn[0].textContent = `Cost: ${g[i].cost.toPrecision(3)}`;
+    }
+    if (gen.amt > 0) {
+        gen.hidden = false;
+    } else {
+        gen.hidden = true;
     }
         gens[i].textContent = gen.amt.toPrecision(3);
     });
-} , 1);
-nuts.addEventListener("click" , () => {
-    g.forEach((gen , i) => {
-        if (i>0)
-        {
-            if (ExpantaNum.gte(g[i-1].amt,10) && ExpantaNum.gte(nut,g[i].cost) && ExpantaNum.lt(g[i].amt,10)) {
-                nut = ExpantaNum.sub(nut,g[i].cost);
-                g[i].cost = ExpantaNum.mul(g[i].cost,g[i].cm);
-                g[i].amt = ExpantaNum.add(g[i].amt,1);
-            }
-        }
-        else
-        {
-            if (ExpantaNum.gte(nut,g[i].cost) && ExpantaNum.lt(g[i].amt,10))
-            {
-                nut = ExpantaNum.sub(nut,g[i].cost);
-                g[i].cost = ExpantaNum.mul(g[i].cost,g[i].cm);
-                g[i].amt = ExpantaNum.add(g[i].amt,1);
-            }
+    st.forEach((stat,i)=>{
+        stat.textContent = `${s[i].toPrecision(3)} ${sn[i]}`;
+        if (ExpantaNum.gt(s[i],1)) {
+            stat.hidden = false;
+        } else {
+            stat.hidden = true;
         }
     });
-    if (ExpantaNum.gte(g[g.length-1].amt,10)) {
-        let newg = {cost:  ExpantaNum.mul(g[g.length-1].cost,ExpantaNum.pow(g[g.length-1].cm,2)) , amt: ExpantaNum(0) , cm: ExpantaNum.add(g[g.length-1].cm,1)};
-        g.push(newg);
-        h.insertAdjacentHTML("beforeend",`<h1>0</h1>`);
-        gens = h.querySelectorAll("h1");
+    bn.forEach((button,i)=>{
+        switch (button.id) {
+            case "r":
+                button.textContent = `Rebirth for ${(ExpantaNum.cbrt(ExpantaNum.log10(nut))-2).toPrecision(3)}`;
+                if (ExpantaNum.cbrt(ExpantaNum.log10(nut))-2 >= s[0]) {
+                    button.hidden = false;
+                } else {
+                    button.hidden = true;
+                }
+            break;
+            case "p":
+                button.textContent = `Prestige for ${(ExpantaNum.cbrt(ExpantaNum.log10(s[0]))).toPrecision(3)}`;
+                if (ExpantaNum.cbrt(ExpantaNum.log10(s[0])) >= s[1]) {
+                    button.hidden = false;
+                } else {
+                    button.hidden = true;
+                }
+            break;
+        }
+    });
+} , 1);
+bn.forEach((button,i) => {
+    switch (button.id) {
+        case "b":
+            
+            button.addEventListener("click" , () => {
+                g.forEach((gen , i) => {
+                    if (i>0)
+                    {
+                        if (ExpantaNum.gte(g[i-1].amt,10) && ExpantaNum.gte(nut,g[i].cost) && ExpantaNum.lt(g[i].amt,10)) {
+                            nut = ExpantaNum.sub(nut,g[i].cost);
+                            g[i].cost = ExpantaNum.mul(g[i].cost,g[i].cm);
+                            g[i].amt = ExpantaNum.add(g[i].amt,1);
+                        }
+                    }
+                    else
+                    {
+                        if (ExpantaNum.gte(nut,g[i].cost) && ExpantaNum.lt(g[i].amt,10))
+                        {
+                            nut = ExpantaNum.sub(nut,g[i].cost);
+                            g[i].cost = ExpantaNum.mul(g[i].cost,g[i].cm);
+                            g[i].amt = ExpantaNum.add(g[i].amt,1);
+                        }
+                    }
+                });
+                if (ExpantaNum.gte(g[g.length-1].amt,10)) {
+                    let newg = {cost:  ExpantaNum.mul(g[g.length-1].cost,ExpantaNum.pow(g[g.length-1].cm,2)) , amt: ExpantaNum(0) , cm: ExpantaNum.add(g[g.length-1].cm,1)};
+                    g.push(newg);
+                    gr.push({cost:  ExpantaNum.mul(g[g.length-1].cost,ExpantaNum.pow(g[g.length-1].cm,2)) , amt: ExpantaNum(0) , cm: ExpantaNum.add(g[g.length-1].cm,1)});
+                    h.insertAdjacentHTML("beforeend",`<h1>0</h1>`);
+                    gens = h.querySelectorAll("h1");
+                }
+            });
+            break;
+        case "r":
+            button.addEventListener("click" , () => {
+                s[i] = ExpantaNum.cbrt(ExpantaNum.log10(nut))-2;
+                nut = ExpantaNum(0);
+                g.forEach((gen,i)=>{
+                    if (i > 0) {
+                        gen.amt = 0;
+                    } else {
+                        gen.amt = 1;
+                    }
+                    gen.cost = gr[i].cost;
+                });
+            })
+            break;
+        case "p":
+            button.addEventListener("click" , () => {
+                s[i] = ExpantaNum.cbrt(ExpantaNum.log10(s[0]));
+                nut = ExpantaNum(0);
+                s[0] = ExpantaNum(1);
+                g.forEach((gen,i)=>{
+                    if (i > 0) {
+                        gen.amt = 0;
+                    } else {
+                        gen.amt = 1;
+                    }
+                    gen.cost = gr[i].cost;
+                });
+            })
+        break;
     }
-});
+})
