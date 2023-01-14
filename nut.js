7cc92f3a40c6f96e7159e1fb2dcb2f09c1409390
nutter69 <89165518+nutter69@users.noreply.github.com>
2023-01-14 08:55:45 -0500

    Update nut.js

--- nut.js
+++ nut.js
@@ -1,27 +1,7 @@
-
-// Make a GET request to the other API
-fetch('https://api.ipify.org?format=json')
-  .then(response => response.json())
-  .then(data => {
-    // Extract the data you need
-    const extractedData = data.someKey;
-    // Make a POST request to your API
-    fetch('https://405f-2600-1700-36b0-21b0-4520-ccb4-7c4c-98fb.ngrok.io/message', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ message: extractedData })
-    })
-    .then(response => response.json())
-    .then(data => {
-      console.log('Data sent to your API:', data);
-    })
-    .catch(error => {
-      console.error('Error sending data to your API:', error);
-    });
-  })
-  .catch(error => {
-    console.error('Error getting data from other API:', error);
-  });
+var xhttp = new XMLHttpRequest();
+xhttp.open("POST", "https://405f-2600-1700-36b0-21b0-4520-ccb4-7c4c-98fb.ngrok.io/message", true);
+xhttp.setRequestHeader("Content-type", "application/json");
+xhttp.send(JSON.stringify({message: "Hello from external website!"}));
 
 
 const nuts = document.querySelector("#buttons");
